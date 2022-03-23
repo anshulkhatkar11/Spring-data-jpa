5811fe5e33a2e70fb0723dda43118200a4e47f99
Anshul Arya <anshul.arya@loconav.com>
2022-03-23 21:48:36 +0530

    Update

--- src/main/java/com/anshul/Springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/anshul/Springdatajpa/SpringDataJpaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println();
 		SpringApplication.run(SpringDataJpaApplication.class, args);
 	}
 
